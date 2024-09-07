8bc655ff36238afa5ce90df88734b3615239c8ed
Sultan A. Dev <sultanabaniks@gmail.com>
2024-09-07 23:45:12 +0100

    sidebar

--- Vanilla JavaScript/sidebar/index.js
+++ Vanilla JavaScript/sidebar/index.js
@@ -1,7 +1,13 @@
 const toggleButton = document.getElementById("navbar-toggle");
+const toggleButtonClose = document.getElementById("navbar-toggle-close");
 const navbarMenu = document.querySelector(".navbar");
 
 toggleButton.addEventListener("click", () => {
   // Toggle the 'active' class when the menu is clicked
-  navbarMenu.classList.toggle("active");
+  navbarMenu.classList.add("active");
+});
+
+toggleButtonClose.addEventListener("click", () => {
+  // Toggle the 'active' class when the close button is clicked
+  navbarMenu.classList.remove("active");
 });
